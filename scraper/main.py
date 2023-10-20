c9d4f74d1b31adddfb32e62b76cbf710d7c07d55
mrochny <tpalinski@icloud.com>
2023-10-20 13:46:49 +0200

    Find categories form the main site

--- scraper/main.py
+++ scraper/main.py
@@ -1,5 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def get_categories_html() -> BeautifulSoup:
+    res = requests.get('https://euro-sterydy.pl/sterydy-sklep/')
+    return BeautifulSoup(res.content, 'html.parser')
+
+def find_categories(soup: BeautifulSoup):
+    category_html = soup.find_all("div", {"class": "name_cat"})
+    for category in category_html:
+        print(category)
+
+
 def main():
     print("Hello, we are gonna steal some data")
-
+    soup = get_categories_html()
+    find_categories(soup)
 
 main()
