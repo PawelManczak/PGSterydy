7414d8c506154b51be96c7c36fbb768e047539af
mrochny <tpalinski@icloud.com>
2023-10-21 12:19:34 +0200

    Move to pandas for csv operations

--- scraper/main.py
+++ scraper/main.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import os
-
+import pandas as pd
 
 def get_categories_html() -> BeautifulSoup:
     res = requests.get('https://euro-sterydy.pl/sterydy-sklep/')
@@ -15,26 +15,21 @@ def find_categories(soup: BeautifulSoup):
         category_name = category.text
         category_link = category.contents[0]['href']
         categories.append([category_name, category_link])
-    return categories
+    return pd.DataFrame.from_records(categories, columns=['Nazwa', 'Adres'])
+    
 
 
 # Save to a .csv, each subarray of data being a separate record
 def save_to_file(file_name, data):
     curr_dir = os.path.abspath(os.getcwd()).split()[0]
     file_path = os.path.join(os.path.dirname(curr_dir), "scraper data", file_name)
-    f = open(file_path, 'w')
-    for record in data:
-        for field in record:
-            f.write(field)
-            f.write("; ")
-        f.write("\n")
-    f.close()
-
+    data.to_csv(file_path, ';')
 
 def main():
     print("Hello, we are gonna steal some data")
     soup = get_categories_html()
     category_data = find_categories(soup)
+    print(category_data)
     save_to_file("categories.csv", category_data)
 
 
