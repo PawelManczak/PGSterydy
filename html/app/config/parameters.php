29a6a2d5b2d807bc514f623777d5fde48b289b47
kasiaOsowska <kasia@osowski.info>
2023-12-04 22:09:50 +0100

    Added prestashop src and db

---
+++ html/app/config/parameters.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'mysql',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'root',
+    'database_password' => 'admin',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'Jz2L4R61uCBOwHxtGW8zNINm18T0jHFRbyJCiGyk3BzWGwipxPKqkv1TYyKeiLus',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2023-12-04',
+    'locale' => 'pl-PL',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'XmqDfTFiqFJ0qmYoG09g0coN46r3bWgNHQSNe1uQbP2cgOEOBMEeB6Key9F5aRR2',
+    'cookie_iv' => 'q879H6GOfi3Be3P3aWXfcwerPrQVbLkj',
+    'new_cookie_key' => 'def000008b26f5d84c27e8b189a7c786b9ff8c13dd3435d2b011cf9b7d0e686c1a3d63f4ab05a39c8ab6e941c638414cce6c5a86b015dd9bfeb3ac98608a63dac90d7c44',
+  ),
+);
